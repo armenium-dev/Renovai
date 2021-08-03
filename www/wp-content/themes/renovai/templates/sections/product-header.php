b9a960721463f57f4abf45e193b0077d8cdf945b
Armen <armen@digidez.com>
2021-08-03 13:40:00 +0200

    35. New site navigation menu (7)

--- www/wp-content/themes/renovai/templates/sections/product-header.php
+++ www/wp-content/themes/renovai/templates/sections/product-header.php
@@ -15,11 +15,11 @@ $section_class = ($section_data['section_desktop_illustration_type'] == 'nothing
 						<h1 class="h1"><?=$section_data['section_title'];?></h1>
 						<p><?=$section_data['section_content'];?></p>
 						<div><?=Functions::render_section_button($section_data['section_button'], ['class' => 'btn btn-secondary shadow']);?></div>
-						<?php if($section_data['section_desktop_illustration_type'] != 'nothing'):?>
 						<div class="mobile-content d-lg-none pt-6 pb-3">
+						<?php if($section_data['section_desktop_illustration_type'] != 'nothing'):?>
 							<img class="w-100 h-auto" src="<?=$section_data['section_mobile_image'];?>" alt="" title="">
-						</div>
 						<?php endif;?>
+						</div>
 						<div class="tablet-container d-none d-lg-inline-block">
 						<?php if($section_data['section_desktop_illustration_type'] == 'image'):?>
 							<img src="<?=$section_data['section_desktop_image'];?>" alt="" title="">
