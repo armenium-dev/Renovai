4aa74d3cc1825963fd056286d54d494a33aa3692
Armen <armen@digidez.com>
2021-08-06 13:18:29 +0200

    36. Integration Section (minor fixed)

--- www/wp-content/themes/renovai/templates/sections/integration.php
+++ www/wp-content/themes/renovai/templates/sections/integration.php
@@ -7,7 +7,7 @@ use Digidez\Helper;
 ?>
 <section id="<?=$section_name;?>-section" class="bg-pink5 pt-lg-7 pt-4 pb-6 siriyp-section">
 	<div class="container">
-		<div class="row mb-lg-6 mb-4">
+		<div class="row mb-lg-6 mb-5">
 			<div class="col-12 col-xxl-7 text-center mx-auto">
 				<h2 class="display-h2"><?=$section_data['section_title'];?></h2>
 			</div>
