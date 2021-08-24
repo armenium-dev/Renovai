8975d9f806ac1b8133b1801894ffc49ba2a9c4b5
Armen <armen@digidez.com>
2021-08-24 19:58:39 +0200

    40. Homepage hero animation

--- www/wp-content/themes/renovai/templates/sections/hero.php
+++ www/wp-content/themes/renovai/templates/sections/hero.php
@@ -18,88 +18,92 @@ use Digidez\Helper;
 			<div class="pillar-center"></div>
 			<div class="pillar-right"></div>
 		</div>
-		<div class="overflow-hidden w-100 h-100 animation-header-slide-01 d-none d-md-block">
-			<div class="woman"></div>
-			<div class="properties-container">
-				<div class="property"><i class="i i-demographic"></i>
-					<div class="property-text"><b>Demographic:</b><span> 21/single</span></div>
+		<div class="animation-scenes">
+			<!--+AnimationHeaderSlide01()-->
+			<div class="animation-scene overflow-hidden w-100 h-100 animation-header-slide-02 animation">
+				<div class="woman"></div>
+				<div class="properties-container">
+					<div class="property"><i class="i i-demographic"></i>
+						<div class="property-text"><b>Demographic:</b><span> 32/single</span></div>
+					</div>
+					<div class="property"><i class="i i-retro"></i>
+						<div class="property-text"><b>Style:</b><span> Retro</span></div>
+					</div>
+					<div class="property"><i class="i i-plush-velvet"></i>
+						<div class="property-text"><b>Materials:</b><span> Plush Velvet</span></div>
+					</div>
+					<div class="property"><i class="i i-dollar"></i>
+						<div class="property-text"><b>Budget:</b><span> $100-$400</span></div>
+					</div>
 				</div>
-				<div class="property"><i class="i i-style"></i>
-					<div class="property-text"><b>Style:</b><span> Preppy</span></div>
-				</div>
-				<div class="property"><i class="i i-materials"></i>
-					<div class="property-text"><b>Materials:</b><span> Leather & Suede</span></div>
-				</div>
-				<div class="property"><i class="i i-budget"></i>
-					<div class="property-text"><b>Budget:</b><span> €100-€400</span></div>
-				</div>
-			</div>
-			<div class="scene-02">
-				<div class="phone-animation-slide-01"></div>
-				<img class="scene-01-img-01" src="<?=IMAGES_URI?>/scene-01-img-01.png" alt="" title=""><img class="scene-01-img-02" src="<?=IMAGES_URI?>/scene-01-img-02.png" alt="" title=""><img class="scene-01-img-03" src="<?=IMAGES_URI?>/scene-01-img-03.png" alt="" title=""><img class="scene-01-img-04" src="<?=IMAGES_URI?>/scene-01-img-04.png" alt="" title=""><img class="scene-01-img-05" src="<?=IMAGES_URI?>/scene-01-img-05.png" alt="" title=""><img class="scene-01-img-06" src="<?=IMAGES_URI?>/scene-01-img-06.png" alt="" title="">
-			</div>
-		</div>
-		<div class="overflow-hidden w-100 h-100 animation-header-slide-02">
-			<div class="woman"></div>
-			<div class="properties-container">
-				<div class="property"><i class="i i-demographic"></i>
-					<div class="property-text"><b>Demographic:</b><span> 32/single</span></div>
-				</div>
-				<div class="property"><i class="i i-retro"></i>
-					<div class="property-text"><b>Style:</b><span> Retro</span></div>
-				</div>
-				<div class="property"><i class="i i-plush-velvet"></i>
-					<div class="property-text"><b>Materials:</b><span> Plush Velvet</span></div>
-				</div>
-				<div class="property"><i class="i i-budget"></i>
-					<div class="property-text"><b>Budget:</b><span> €100-€400</span></div>
+				<div class="scene-02">
+					<div class="phone-animation-slide-02"></div>
+					<img class="scene-02-img-02" src="<?=IMAGES_URI?>/scene-02-img-02.png" alt="" title=""><img class="scene-02-img-03" src="<?=IMAGES_URI?>/scene-02-img-03.png" alt="" title=""><img class="scene-02-img-04" src="<?=IMAGES_URI?>/scene-02-img-04.png" alt="" title=""><img class="scene-02-img-05" src="<?=IMAGES_URI?>/scene-02-img-05.png" alt="" title=""><img class="scene-02-img-01" src="<?=IMAGES_URI?>/scene-02-img-01.png" alt="" title="">
 				</div>
 			</div>
-			<div class="scene-02">
-				<div class="phone-animation-slide-02"></div>
-				<img class="scene-02-img-02" src="<?=IMAGES_URI?>/scene-02-img-02.png" alt="" title=""><img class="scene-02-img-03" src="<?=IMAGES_URI?>/scene-02-img-03.png" alt="" title=""><img class="scene-02-img-04" src="<?=IMAGES_URI?>/scene-02-img-04.png" alt="" title=""><img class="scene-02-img-05" src="<?=IMAGES_URI?>/scene-02-img-05.png" alt="" title=""><img class="scene-02-img-01" src="<?=IMAGES_URI?>/scene-02-img-01.png" alt="" title="">
-			</div>
-		</div>
-		<div class="overflow-hidden w-100 h-100 animation-header-slide-03 d-none d-md-block">
-			<div class="man"></div>
-			<div class="properties-container">
-				<div class="property"><i class="i i-demographic"></i>
-					<div class="property-text"><b>Demographic:</b><span> 26/single</span></div>
+			<!--+AnimationHeaderSlide03()-->
+			<div class="animation-scene overflow-hidden w-100 h-100 animation-header-slide-04">
+				<div class="man"></div>
+				<div class="properties-container">
+					<div class="property"><i class="i i-demographic"></i>
+						<div class="property-text"><b>Demographic:</b><span> 51/single</span></div>
+					</div>
+					<div class="property"><i class="i i-industrial"></i>
+						<div class="property-text"><b>Style:</b><span> Industrial</span></div>
+					</div>
+					<div class="property"><i class="i i-steel-stone-marble"></i>
+						<div class="property-text"><b>Materials:</b><span> Steel/Stone/Marble</span></div>
+					</div>
+					<div class="property"><i class="i i-dollar"></i>
+						<div class="property-text"><b>Budget:</b><span> $400-$800</span></div>
+					</div>
 				</div>
-				<div class="property"><i class="i i-minimal"></i>
-					<div class="property-text"><b>Style:</b><span> Minimal</span></div>
-				</div>
-				<div class="property"><i class="i i-organic-cotton"></i>
-					<div class="property-text"><b>Materials:</b><span> Organic Cotton</span></div>
-				</div>
-				<div class="property"><i class="i i-budget"></i>
-					<div class="property-text"><b>Budget:</b><span> €100-€400</span></div>
-				</div>
-			</div>
-			<div class="scene-02">
-				<div class="phone-animation-slide-03"></div>
-				<img class="scene-03-img-02" src="<?=IMAGES_URI?>/scene-03-img-02.png" alt="" title=""><img class="scene-03-img-01" src="<?=IMAGES_URI?>/scene-03-img-01.png" alt="" title=""><img class="scene-03-img-03" src="<?=IMAGES_URI?>/scene-03-img-03.png" alt="" title=""><img class="scene-03-img-04" src="<?=IMAGES_URI?>/scene-03-img-04.png" alt="" title=""><img class="scene-03-img-05" src="<?=IMAGES_URI?>/scene-03-img-05.png" alt="" title=""><img class="scene-03-img-06" src="<?=IMAGES_URI?>/scene-03-img-06.png" alt="" title="">
-			</div>
-		</div>
-		<div class="overflow-hidden w-100 h-100 animation-header-slide-04 d-none d-md-block">
-			<div class="man"></div>
-			<div class="properties-container">
-				<div class="property"><i class="i i-demographic"></i>
-					<div class="property-text"><b>Demographic:</b><span> 51/married +2</span></div>
-				</div>
-				<div class="property"><i class="i i-industrial"></i>
-					<div class="property-text"><b>Style:</b><span> Industrial</span></div>
-				</div>
-				<div class="property"><i class="i i-steel-stone-marble"></i>
-					<div class="property-text"><b>Materials:</b><span> Steel/Stone/Marble</span></div>
-				</div>
-				<div class="property"><i class="i i-budget"></i>
-					<div class="property-text"><b>Budget:</b><span> €200-€600</span></div>
+				<div class="scene-02">
+					<div class="phone-animation-slide-04"></div>
+					<img class="scene-04-img-02" src="<?=IMAGES_URI?>/scene-04-img-02.png" alt="" title=""><img class="scene-04-img-03" src="<?=IMAGES_URI?>/scene-04-img-03.png" alt="" title=""><img class="scene-04-img-05" src="<?=IMAGES_URI?>/scene-04-img-05.png" alt="" title=""><img class="scene-04-img-01" src="<?=IMAGES_URI?>/scene-04-img-01.png" alt="" title=""><img class="scene-04-img-04" src="<?=IMAGES_URI?>/scene-04-img-04.png" alt="" title="">
 				</div>
 			</div>
-			<div class="scene-02">
-				<div class="phone-animation-slide-04"></div>
-				<img class="scene-04-img-02" src="<?=IMAGES_URI?>/scene-04-img-02.png" alt="" title=""><img class="scene-04-img-03" src="<?=IMAGES_URI?>/scene-04-img-03.png" alt="" title=""><img class="scene-04-img-05" src="<?=IMAGES_URI?>/scene-04-img-05.png" alt="" title=""><img class="scene-04-img-01" src="<?=IMAGES_URI?>/scene-04-img-01.png" alt="" title=""><img class="scene-04-img-04" src="<?=IMAGES_URI?>/scene-04-img-04.png" alt="" title="">
+			<div class="animation-scene overflow-hidden w-100 h-100 animation-header-slide-05">
+				<div class="woman"></div>
+				<div class="properties-container">
+					<div class="property"><i class="i i-demographic"></i>
+						<div class="property-text"><b>Demographic:</b><span> 53/married</span></div>
+					</div>
+					<div class="property"><i class="i i-boho"></i>
+						<div class="property-text"><b>Style:</b><span> Boho</span></div>
+					</div>
+					<div class="property"><i class="i i-rattan-wood"></i>
+						<div class="property-text"><b>Materials:</b><span> Rattan & Wood</span></div>
+					</div>
+					<div class="property"><i class="i i-dollar"></i>
+						<div class="property-text"><b>Budget:</b><span> €900-€2000</span></div>
+					</div>
+				</div>
+				<div class="scene-05">
+					<div class="phone-animation-slide-05"></div>
+					<img class="scene-05-img-02" src="<?=IMAGES_URI?>/scene-05-img-02.png" alt="" title=""><img class="scene-05-img-03" src="<?=IMAGES_URI?>/scene-05-img-03.png" alt="" title=""><img class="scene-05-img-01" src="<?=IMAGES_URI?>/scene-05-img-01.png" alt="" title=""><img class="scene-05-img-04" src="<?=IMAGES_URI?>/scene-05-img-04.png" alt="" title="">
+				</div>
+			</div>
+			<div class="animation-scene overflow-hidden w-100 h-100 animation-header-slide-06">
+				<div class="man"></div>
+				<div class="properties-container">
+					<div class="property"><i class="i i-demographic"></i>
+						<div class="property-text"><b>Demographic:</b><span> 51/married +2</span></div>
+					</div>
+					<div class="property"><i class="i i-industrial"></i>
+						<div class="property-text"><b>Style:</b><span> Industrial</span></div>
+					</div>
+					<div class="property"><i class="i i-steel-stone-marble"></i>
+						<div class="property-text"><b>Materials:</b><span> Steel/Stone/Marble</span></div>
+					</div>
+					<div class="property"><i class="i i-dollar"></i>
+						<div class="property-text"><b>Budget:</b><span> €200-€600</span></div>
+					</div>
+				</div>
+				<div class="scene-06">
+					<div class="phone-animation-slide-06"></div>
+					<img class="scene-04-img-02" src="<?=IMAGES_URI?>/scene-06-img-02.png" alt="" title=""><img class="scene-04-img-03" src="<?=IMAGES_URI?>/scene-06-img-03.png" alt="" title=""><img class="scene-04-img-01" src="<?=IMAGES_URI?>/scene-06-img-01.png" alt="" title=""><img class="scene-04-img-04" src="<?=IMAGES_URI?>/scene-06-img-04.png" alt="" title="">
+				</div>
 			</div>
 		</div>
 		<div class="animation-plus-container">
